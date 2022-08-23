1ed249d10264f0b33ec00f720f20d5285f39ee0c
meron <meron@example.com>
2022-08-23 05:58:03 -0700

    first try on task 4

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+for n in range(0, 100):
+    if (n < 10):
+        print("0{}, ".format(n), end="")
+    elif (n == 99):
+        print("{}".format(n))
+    else:
+        print("{}, ".format(n), end="")
