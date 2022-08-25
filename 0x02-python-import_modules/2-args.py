cb15a4a8028386e4d7375e3e17fa9b9cdeb340b8
meron <meron@example.com>
2022-08-25 10:27:37 -0700

    task 2 first try

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+
+    import sys
+    num_arg = len(sys.argv) - 1
+
+    if num_arg == 0:
+        print("0 arguments.")
+    else:
+        if num_arg == 1:
+            print("1 argument:")
+        elif num_arg > 1:
+            print("{} arguments:".format(num_arg))
+    for i in range(num_arg):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
