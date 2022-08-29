0953000f98ba8191be18cb138a9e1dffcfaf4509
meron <meron@example.com>
2022-08-29 10:19:49 -0700

    if this is the thing that is causing the error you have problems

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -4,6 +4,6 @@ def max_integer(my_list=[]):
         return None
     start = my_list[0]
     for i in range(len(my_list) - 1):
-        if start < my_list[i]:
+        if my_list[i] > start:
             start = my_list[i]
     return start
