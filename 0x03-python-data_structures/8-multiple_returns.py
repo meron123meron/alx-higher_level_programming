38f79431041f7159e16d315d1ea2e4560b17150e
meron <meron@example.com>
2022-08-29 09:42:12 -0700

    fourth try because of check 6 error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_a = len(sentence), sentence[0]
+
     if sentence == "":
         return (0, None)
-    return tuple_a
+    else:
+        return (len(sentence), sentence[0])
