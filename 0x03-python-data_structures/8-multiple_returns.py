e2ce215cb78bf65b65725b9800a089cca575c327
meron <meron@example.com>
2022-08-29 09:32:01 -0700

    third try

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    tuple_a = (len(sentence), sentence[0])
+    tuple_a = len(sentence), sentence[0]
     if sentence == "":
         return (0, None)
     return tuple_a
