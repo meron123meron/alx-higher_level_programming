bdbfc6567ee17b0bb6ed07cc6e0dda9058bd5576
meron <meron@example.com>
2022-08-31 08:38:53 -0700

    indentation error

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,6 @@
 def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
-        
     else:
         start = list(a_dictionary.keys())[0]
         for key in a_dictionary:
