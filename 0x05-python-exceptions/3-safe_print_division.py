67f45657eb60689239a0462d3ce015ecd00f5c87
meron <meron@example.com>
2022-09-13 05:47:49 -0700

    second try

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         div = a / b
-    except:
+    except (TypeError, ZeroDivisionError):
         div = None
     finally:
         print("Inside result: {}".format(div))
