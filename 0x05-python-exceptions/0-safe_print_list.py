0f562aff60abe3445751d10053969b22ce686459
meron <meron@example.com>
2022-09-13 03:44:14 -0700

    second try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    c = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            if i == x - 1:
-                print("")
+            c = c + 1
         except IndexError:
             break
-    return (i + 1)
+    print()
+    return c
