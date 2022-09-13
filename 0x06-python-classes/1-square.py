f7d2022c9aeb69d9d75723e6f6684919c18ec7a5
meron <meron@example.com>
2022-09-13 13:20:38 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""define a class square"""
+class square:
+    
+    """a class with attribute"""
+
+    def __init__(self, size):
+
+        """ size (int): The size of the new square."""
+        self.__size = size
