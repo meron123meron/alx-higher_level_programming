ff2a48f29ddd58ac4320722f4d5b208163213d14
meron <meron@example.com>
2022-09-14 07:43:36 -0700

    please bug show yourself

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -4,6 +4,7 @@ def list_division(my_list_1, my_list_2, list_length):
 
     new_list = []
     for i in range(list_length):
+
         try:
             div = my_list_1[i] / my_list_2[i]
         except TypeError:
@@ -17,4 +18,5 @@ def list_division(my_list_1, my_list_2, list_length):
             div = 0
         finally:
             new_list.append(div)
+
     return new_list
