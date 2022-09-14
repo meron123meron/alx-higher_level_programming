9c395e1e2d90b54974fb9555daf990bb107d59b8
meron <meron@example.com>
2022-09-14 07:21:31 -0700

    fifth try

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+
 def list_division(my_list_1, my_list_2, list_length):
+    """divides element by element 2 lists"""
+
     new_list = []
     for i in range(list_length):
         try:
