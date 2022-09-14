3062095a231867b1b0353620ff6fa6e6c76f9c76
meron <meron@example.com>
2022-09-14 12:59:00 -0700

    pycodestyle error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,24 +5,24 @@
 
 class Square:
 
-        """is a class"""
+    """is a class"""
 
-        def __init__(self, size=0):
+    def __init__(self, size=0):
 
-            """Initialize a new Square.
+        """Initialize a new Square.
 
-            Args:
+        Args:
 
-            size (int): The size of the new square.
+        size (int): The size of the new square.
 
-            """
+        """
 
-            if not isinstance(size, int):
+        if not isinstance(size, int):
 
-                raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
 
-            elif size < 0:
+        elif size < 0:
 
-                raise ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
 
-            self.__size = size
+        self.__size = size
