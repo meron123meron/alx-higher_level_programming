8a7ba0b33c7c7e59fcf8d982a1dec49f0789ab88
meron <meron@example.com>
2022-09-18 03:15:24 -0700

    task 4 first trial

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This is 5-text_indentation module"""
+def text_indentation(text):
+    """prints a text with 2 new lines
+    after . ? and :"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    flag = 0
+    for i in text:
+        if flag == 0:
+            if i == " ":
+                continue
+            else:
+                flag = 1
+        if flag == 1:
+            if i == "." or "?" or ":":
+                print(i)
+                print()
+                flag = 0
+            else:
+                print(i, end="")
