9288f89a3aef64208b78b0a3f292bff4082ea4c4
meron <meron@example.com>
2022-09-18 12:01:45 -0700

    nothing huge

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -12,5 +12,5 @@ import numpy
 
 def lazy_matrix_mul(m_a, m_b):
 
-    """ calculates the matrix multiplication of two matrices"""
+    """ calculates multiplication of two matrices"""
     return numpy.matmul(m_a, m_b)
