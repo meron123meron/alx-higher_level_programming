d752b9ad82fd2392e131f130c164c022b326086e
meron <meron@example.com>
2022-09-18 01:38:30 -0700

    task 2 first trial

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This is 3-say_my_name module"""
+def say_my_name(first_name, last_name=""):
+    """prints the first and last names"""
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
