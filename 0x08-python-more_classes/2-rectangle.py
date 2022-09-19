0f174f308bd6360dc97d8664e61ed84bd956f604
meron <meron@example.com>
2022-09-19 15:04:28 -0700

    forgot one if condition

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -44,4 +44,6 @@ class Rectangle:
 
     def perimeter(self):
         """calculates the perimeter of the rect"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width * 2) + (self.__height * 2)
