e3b14590ba65d478dc2aaf134f7b240551d9ae72
meron <meron@example.com>
2022-09-19 14:29:38 -0700

    third try

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@ class Rectangle:
 
     """is a class"""
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
