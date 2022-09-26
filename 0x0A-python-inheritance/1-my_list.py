5fecf8f646e31a6501730be0f9ff24be98c5bdc2
meron <meron@example.com>
2022-09-26 07:54:45 -0700

    third try

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,9 @@
 
 class MyList(list):
     """a class that inherits from list"""
+    def __init__(self):
+        """initialization"""
+        super().__init__()
 
     def print_sorted(self):
         """print the list in sorted order"""
