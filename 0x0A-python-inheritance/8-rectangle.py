57c9c65b47988479c75892c60b139b120a139b83
meron <meron@example.com>
2022-09-26 13:53:07 -0700

    second trial

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,25 +4,9 @@ Defines a class
 """
 
 
-class BaseGeometry:
-    """A class"""
-    def area(self):
-        """public instance method"""
-        raise Exception("area() is not implemented")
-    def integer_validator(self, name, value):
-        """public instance method
-        Validates value to be an int grater than 0.
-        Args:
-        name (str): name of the value
-        value (unknown): to be validated.
-        Raises:
-        TypeError: if value is not an int.
-        ValueError: if value is <= 0.
-        """
-        if type(value) is not int:
-            raise TypeError("<name> must be an integer")
-        if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """inherits from BaseGeometry"""
     def __init__(self, width, height):
