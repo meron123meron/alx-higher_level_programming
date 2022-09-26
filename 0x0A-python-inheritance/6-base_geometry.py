04c18a0b10d68731ebbcdc454366d08992bfbd32
meron <meron@example.com>
2022-09-26 13:02:15 -0700

    task 6 first try

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines a class
+"""
+
+
+class BaseGeometry:
+    """A class"""
+    def area(self):
+        """public instance"""
+        raise Exception("area() is not implemented")
