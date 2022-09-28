1204e434bbf28b9bba7e972c8d4e1e96e845c909
meron <meron@example.com>
2022-09-28 02:45:35 -0700

    Docstring problem

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Define a file reading function"""
+
+
 def read_file(filename=""):
     """reads a text file"""
     with open(filename, encoding="utf-8") as myfile:
--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Defines a file writing function"""
+
+
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and
+    """writes a string to a text file (UTF8)
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
     returns the number of characters written
     """
     with open(filename, mode="w", encoding="utf-8") as myfile:
