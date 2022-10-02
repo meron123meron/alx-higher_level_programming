63d9b2d8ee0f8f1b1de9dbd5e94b3e926391b0df
meron <meron@example.com>
2022-10-02 04:44:44 -0700

    i forgot getter and setter on task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -19,3 +19,35 @@ class Rectangle(Base):
         self.__height = height
         self.__x = x
         self.__y = y
+    @property
+    def width(self):
+        """getter"""
+        return self.__width
+    @width.setter
+    def width(self, value):
+        """setter"""
+        self.__width = value
+    @property
+    def height(self):
+        """getter"""
+        return self.height
+    @height.setter
+    def height(self, value):
+        """setter"""
+        self.__height = value
+    @property
+    def x(self):
+        """getter"""
+        return self.__x
+    @x.setter
+    def x(self, value):
+        """setter"""
+        self.__x = value
+    @property
+    def y(self):
+        """getter"""
+        return self.__y
+    @y.setter
+    def y(self, value):
+        """setter"""
+        self.__y = value
