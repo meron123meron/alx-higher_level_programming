43d8c78af5953d391d511bb9b3f99b3b0e08bb82
meron <meron@example.com>
2022-11-03 09:10:55 -0700

    second try

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -2,4 +2,4 @@
 -- Don’t list rows without a name value
 -- Results should display the score and the name (in this order)
 -- Records should be listed by descending score
-SELECT score, name FROM second_table WHERE name NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
