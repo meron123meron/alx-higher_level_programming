b6967ff2af151ccb9c88e4cae229ebc7ac609212
meron <meron@example.com>
2022-11-30 11:07:52 +0000

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const n = Number(process.argv[2]);
+if (isNaN(n)) {
+  console.log('Missing number of occurrences');
+}
+for (let i = 0; i < n; i++) {
+  console.log('C is fun');
+};
