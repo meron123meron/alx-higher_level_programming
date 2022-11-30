ee0b98543bf505733420ae771dfc9250f19f0b1c
meron <meron@example.com>
2022-11-30 15:16:44 +0000

    third trial

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-const n = Number(process.argv[2]);
+const n = Math.floor(Number(process.argv[2]));
 if (isNaN(n)) {
   console.log('Missing size');
-} else {
-  for (let i = 0; i < n; i++) {
-    let r = '';
-    for (let j = 0; j < n; j++) {
-      r = r + 'x';
-    }
-    console.log(r);
+}
+for (let i = 0; i < n; i++) {
+  let r = '';
+  for (let j = 0; j < n; j++) {
+    r = r + 'x';
   }
+  console.log(r);
 }
