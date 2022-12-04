fc02193aab7322be68212c2203f3788d9df060c9
meron nigussie <merontafer@gmail.com>
2022-12-04 14:43:29 +0300

    third try

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM `states` WHERE\
-              BINARY `name` = '%s' ORDER BY states.id ASC".format(sys.argv[4]))
+             `name` = '%s' ORDER BY states.id ASC".format(sys.argv[4]))
     rows = c.fetchall()
     for row in rows:
         print(row)
