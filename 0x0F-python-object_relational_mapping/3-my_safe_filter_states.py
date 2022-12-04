4004005a922bf37807ce7839466e34de73d64b27
meron nigussie <merontafer@gmail.com>
2022-12-04 14:40:06 +0300

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+The same as task 2 But this time, write one that is safe from MySQL injections!
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states` WHERE\
+              BINARY `name` = '%s' ORDER BY states.id ASC".format(sys.argv[4]))
+    rows = c.fetchall()
+    for row in rows:
+        print(row)
+    db.close()
