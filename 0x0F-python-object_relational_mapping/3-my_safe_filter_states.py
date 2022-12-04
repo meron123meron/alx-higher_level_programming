511ecf56697535adbf731227e135d633aed2468a
meron nigussie <merontafer@gmail.com>
2022-12-04 15:06:08 +0300

    sixth try

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -6,12 +6,11 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-     db = MySQLdb.connect(host='localhost', port=3306,
+    db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name=%s\
-                ORDER BY states.id ASC", (sys.argv[4],))
+    c.execute("SELECT * FROM states WHERE name LIKE %s\
+              ORDER BY states.id ASC", (sys.argv[4],))
     rows = c.fetchall()
     for row in rows:
         print(row)
