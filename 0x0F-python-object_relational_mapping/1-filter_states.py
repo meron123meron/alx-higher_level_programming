4e10de98eab06fd9319dbaee80d219faf930bc5e
meron nigussie <merontafer@gmail.com>
2022-12-04 12:05:33 +0300

    EOF error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'
-              ORDER BY states.id ASC")
+    c.execute("SELECT * FROM states WHERE name
+              LIKE BINARY 'N%' ORDER BY states.id ASC")
     rows = c.fetchall()
     for row in rows:
         print(row)
