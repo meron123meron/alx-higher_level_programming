2cf04fccb4d3fb433cca5bd982a41954d710272b
meron <meron@example.com>
2022-12-20 21:16:33 +0000

    task 5

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,7 @@ Python script that fetches https://alx-intranet.hbtn.io/status
 """
 import requests
 
+
 if __name__ == "__main__":
     fetch = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
