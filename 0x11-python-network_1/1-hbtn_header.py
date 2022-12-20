4ac559b16459af55c8f3f1b8d51efb69c486c33c
meron <meron@example.com>
2022-12-20 22:21:12 +0000

    editing

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,6 +6,7 @@ Python script that takes in a URL, sends a request to the URL and displays the v
 from urllib.request import urlopen
 import sys
 
+
 if __name__ == "__main__":
     url = sys.argv[1]
     with urlopen(url) as response:
