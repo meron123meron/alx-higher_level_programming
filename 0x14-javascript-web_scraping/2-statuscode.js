de4af915196c5c91e37f9e9955f344e59787db18
meron <meron@example.com>
2023-01-21 18:43:19 +0000

    space

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,7 +4,7 @@ const request = require('request');
 const URL = process.argv[2];
 
 request(URL, function (err, response) {
-if (err) {
+  if (err) {
     console.log(err);
   } else {
     console.log('code: ' + response.statusCode);
