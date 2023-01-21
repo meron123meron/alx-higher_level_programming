9deba817b893cd4af55f2a8578b7543481ce42b6
meron <meron@example.com>
2023-01-21 16:12:23 +0000

    fourth trial

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,6 @@
 const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', function (err, content) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(content);
-  }
+fs.readFile(file, 'utf-8', function (err, data) {
+ console.log(err || data);
 });
