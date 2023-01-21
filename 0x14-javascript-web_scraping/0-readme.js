4a4e68cf97b76068ddd3eb17b9f5d04ac3410420
meron <meron@example.com>
2023-01-21 16:16:38 +0000

    I restored my previous commit

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,6 +3,10 @@
 const fs = require('fs');
 const file = process.argv[2];
 
-fs.readFile(file, 'utf-8', function (err, data) {
- console.log(err || data);
+fs.readFile(file, 'utf-8', function (err, content) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(content);
+  }
 });
