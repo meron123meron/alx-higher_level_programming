7cb544a37603116744382cfef5b7d0f9a9a2f043
meron <meron@example.com>
2023-01-21 19:41:16 +0000

    second ...

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -9,6 +9,6 @@ request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else {
-    fs.writeFile(filePath, body, 'utf-8');
+    fs.createWriteStream(filePath);
   }
 });
